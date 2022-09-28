0d206704e6d4c5731ebba09bab83d6fa9f24f19e
Ana Carolina Oliveira <carol.oliva.cruz@gmail.com>
2022-09-27 23:12:41 -0300

    atualização

--- src/scripts/metodos.js
+++ src/scripts/metodos.js
@@ -17,7 +17,7 @@ meuMetodoforEach(array, minhaCallBackForEach)
 // Map
 function minhaCallBackMap(element, index, array) {
     return `Número ${element} no index: ${index}, veio desse array: ${array}`;
-  }
+}
 
 function meuMetodoMap(array, callback) {
     let newArray = []
@@ -32,17 +32,17 @@ function meuMetodoMap(array, callback) {
 
 // Filter
 function minhaCallBackFilter(element, i, array) {
-   
-   if(element > 2 && i && array.length > 2){
-    return true
-   }
-   
+
+    if (element > 2 && i && array.length > 2) {
+        return true
+    }
+
 }
 
 function meuMetodoFilter(array, callback) {
     let newArray = []
     for (i = 0; i < array.length; i++) {
-        if(callback(array[i], i, array)){
+        if (callback(array[i], i, array)) {
             newArray.push(array[i])
         }
     }
@@ -52,19 +52,20 @@ function meuMetodoFilter(array, callback) {
 
 
 // Reduce
-function minhaCallBackReduce(element, i, total) {
+let total = 0
+function minhaCallBackReduce(element) {
     total = total += element
-}
-
-function meuMetodoReduce(array){
-    let total = 0
-
-    for (let i = 0; i < array.length; i++) {
-        elements = array[i]
-        total += elements
-    }
     return total
 }
-// console.log(meuMetodoReduce(array, meuMetodoReduce))
+
+function meuMetodoReduce(array, callback) {
+
+    for (let i = 0; i < array.length; i++) {
+        element = array[i]
+        callback(element)
+    }
+   
+}
+console.log(meuMetodoReduce(array, minhaCallBackReduce))
 
 
